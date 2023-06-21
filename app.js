4aadde08ae23e198739fb11c760ac1726e70d96d
MokhtarAkle <mokhtar.akle@hva.nl>
2023-06-21 03:06:12 +0200

    added unique users, ui work, json file api and select options etc.

--- app.js
+++ app.js
@@ -1,28 +1,59 @@
-var express = require('express');
-var path = require('path');
+import express from 'express';
+import path from 'path';
 var app = express();
-var http = require('http');
+import * as http from 'http';
 var port = process.env.PORT || 3000;
 var server = http.createServer(app);
-
-
-require('dotenv').config()
+import { fileURLToPath } from 'url';
+import 'dotenv/config'
+import data from './public/exampleapi.json' assert { type: "json" };
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.set('port', port);
 
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/', function(req, res, next) {
-      res.render('index')
+    res.render('index', {data: data})
   });
 
+app.post('/', (request, response) => {
+
+  const url = './public/exampleapi.json'
+    postJson(url, request.body).then((data) => {
+      let newPreset = { ... request.body}
+      console.log(JSON.stringify(request.body));
+     if (data.success) {
+          response.redirect('/presetSaved')
+      }
+      else {
+      const errorMessage = data.message
+  
+      }
+    })
+  })
 server.listen(port, () => {
     console.log('listening on http://localhost:' + port);
   
   });
 
-module.exports = app;
+
+
+
+  async function postJson(url, body) {
+    return await fetch(url, {
+      method: 'post',
+      body: JSON.stringify(body),
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then((response) => response.json())
+      .catch((error) => error)
+  }
+
+export default app;
--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,55 +3,103 @@ let colorpreview = document.querySelector("#colorpreview");
 let onswitch = document.querySelector("#switch");
 let rangeslider = document.querySelector("#rangeSlider");
 let texter = document.querySelector("#textdisplayer");
+let bodyBG = document.querySelector("body")
+let listItems = document.querySelectorAll("select")
+let numbered = document.querySelector("#numberCount");
+const clientId = 'User_' + Math.random().toString(16).substr(2, 8)
+    const options = {
+      keepalive: 60,
+      clientId: clientId,
+      protocolId: 'MQTT',
+      protocolVersion: 4,
+      clean: true,
+      reconnectPeriod: 1000,
+      connectTimeout: 30 * 1000,
+    }
+const client  = mqtt.connect('ws://broker.emqx.io:8083/mqtt', options)
 
-const client  = mqtt.connect('ws://broker.emqx.io:8083/mqtt')
+
+async function initializeSelects(){
+  const requestURL = './exampleapi.json';
+  const request = new Request(requestURL);
+
+  const response = await fetch(request);
+  const ledapi = await response.json();
+  console.log(ledapi);
 
 
+}
+initializeSelects();
 
+
+    console.log(clientId)
   client.on('connect', function () {
+    client.subscribe('status/client-id', function (err) {
+
+    })
     client.subscribe('wled/9df798', function (err) {
-      if (!err) {
-        client.publish('wled/9df798', colorpicker.value)
-      }
+
     })
     client.subscribe('wled/9df798/api', function (err) {
-        if (!err) {
-          client.publish('wled/9df798/api', 'FX=23')
-        }
+
       })
     client.subscribe('wled/9df798/col', function (err) {
-        if (!err) {
-          client.publish('wled/9df798/col', colorpicker.value)
-        }
+
       })   
   })
-  
+  let dynId;
+  let dynerId;
   client.on('message', function (topic, message) {
     // message is Buffer
+
     console.log(topic + " " + message.toString())
-    textinChat(message);
+
+      textinChat(message);
       colorpreview.style.backgroundColor = message;
 
   })
 
 function textinChat (color){
-  const textColor = document.createElement("p");
-textColor.innerHTML = "User selected: " + color;
-document.querySelector("#textdisplayer").appendChild(textColor);
+  const variableValue = document.createElement("p");
+variableValue.innerHTML = color;
+document.querySelector("#textdisplayer").appendChild(variableValue);
 texter.scrollTop = texter.scrollHeight;
 }
 
-colorpicker.addEventListener("input", () => {client.publish('wled/9df798/col', colorpicker.value);
+function userSetup(user){
+  const dynamicID = document.createElement("p");
+  dynamicID.innerHTML = user + " selected: ";
+  document.querySelector("#textdisplayer").appendChild(dynamicID);
+}
 
+colorpicker.addEventListener("change", () => {
+  client.publish('status/client-id', clientId + " Selected color: ");
+  client.publish('wled/9df798/col', colorpicker.value);
 });
 onswitch.addEventListener('change', function() {
   if (this.checked) {
+    client.publish('status/client-id', clientId + " Selected status: ");
     client.publish('wled/9df798', "ON");
   } else {
+    client.publish('status/client-id', clientId + " Selected status: ");
     client.publish('wled/9df798', "OFF");
   }
 });
 
-rangeslider.addEventListener('change', () => {client.publish('wled/9df798/api', 'bri=' + rangeslider.value)
-textinChat(rangeslider);
+rangeslider.addEventListener('change', () => {
+  client.publish('status/client-id', clientId + " Selected brightness: ");
+  client.publish('wled/9df798/api', rangeslider.value)
+});
+
+for(let i = 0; i < listItems.length; i++){
+  listItems[i].addEventListener("change", () =>{
+    client.publish('status/client-id', clientId + " Selected: ");
+    client.publish('wled/9df798/api', listItems[i].value)
+
+  })
+}
+
+numbered.addEventListener('change', () => {
+  client.publish('status/client-id', clientId + " Selected transition time: ");
+  client.publish('wled/9df798/api', numbered.value)
 });
